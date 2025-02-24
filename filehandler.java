a321f1f0deedf14d2ffda07657a8818c546ee466
Cyber Sentinel <55408727+Cyber-Sentinel443@users.noreply.github.com>
2025-02-24 09:35:02 -0600

    Add files via upload

--- filehandler.java
+++ filehandler.java
@@ -1,67 +1,176 @@
 import java.io.*;
 import java.util.*;
 
-public class filehandler {
-    public static void main(String[] args) {
-        // Define the arrays
-        int[] intArray1 = {1, 2, 3, 4, 5};
-        int[] intArray2 = {10, 20, 30, 40, 50};
-        String[] stringArray1 = {"apple", "banana", "cherry"};
-        String[] stringArray2 = {"dog", "elephant", "fox"};
+class FileManger {
 
-        String filename = "data.txt";
+    //consider adding a save option
 
-        // Write arrays to file
-        writeArraysToFile(filename, intArray1, intArray2, stringArray1, stringArray2);
+    //implemnet "void() {} "
+    //This methos is automatically called when started. This could be used to pull all information orginally
 
-        // Read arrays from file
-        readArraysFromFile(filename);
-    }
+    //Need to add all of the schedule modifiers
 
-    public static void writeArraysToFile(String filename, int[] intArr1, int[] intArr2, String[] strArr1, String[] strArr2) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            // Write integer arrays
-            writer.write(Arrays.toString(intArr1) + "\n");
-            writer.write(Arrays.toString(intArr2) + "\n");
+    //Need to build a searching and organization system for all events
 
-            // Write string arrays
-            writer.write(Arrays.toString(strArr1) + "\n");
-            writer.write(Arrays.toString(strArr2) + "\n");
+    //When this is working, come edit and try to condense code. All the "if" statements are not nessary for this file to run
+    //Change the the program to look for something else besides "" Maybe something like &*& (something not normally used)
 
-            System.out.println("Arrays successfully written to file!");
+    //Possible consider imbedding the class within another class
+    //Try the extend method but I don't think that will work
+
+    private int[] intArr1;
+    private int[] intArr2;
+    private String[] strArr1;
+    private String[] strArr2;
+
+    public void writeArraysToFile() {
+
+        System.out.println("Saving Information");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("appointments.txt"))) {
+            int maxLength = Math.max(Math.max(intArr1.length, intArr2.length), Math.max(strArr1.length, strArr2.length));
+
+            for (int i = 0; i < maxLength; i++) {
+                String col1 = (i < intArr1.length) ? String.valueOf(intArr1[i]) : "";
+                String col2 = (i < intArr2.length) ? String.valueOf(intArr2[i]) : "";
+                String col3 = (i < strArr1.length) ? "\"" + strArr1[i] + "\"" : "";  // Wrap multi-word strings in quotes
+                String col4 = (i < strArr2.length) ? "\"" + strArr2[i] + "\"" : "";  // Wrap multi-word strings in quotes
+
+                writer.write(col1 + " " + col2 + " " + col3 + " " + col4 + "\n");
+            }
+
+            System.out.println("Scheduled Saved");
         } catch (IOException e) {
             System.err.println("Error writing to file: " + e.getMessage());
         }
     }
 
-    public static void readArraysFromFile(String filename) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-            // Read and parse integer arrays
-            int[] intArr1 = parseIntArray(reader.readLine());
-            int[] intArr2 = parseIntArray(reader.readLine());
+    public void readArraysFromFile() {
+        System.out.println("Pulling Schedule");
+        try (BufferedReader reader = new BufferedReader(new FileReader("appointments.txt"))) {
+            List<Integer> intList1 = new ArrayList<>();
+            List<Integer> intList2 = new ArrayList<>();
+            List<String> strList1 = new ArrayList<>();
+            List<String> strList2 = new ArrayList<>();
 
-            // Read and parse string arrays
-            String[] strArr1 = parseStringArray(reader.readLine());
-            String[] strArr2 = parseStringArray(reader.readLine());
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Use regex to properly split numbers and quoted strings
+                List<String> parts = new ArrayList<>();
+                Scanner scanner = new Scanner(line);
+                while (scanner.hasNext()) {
+                    if (scanner.hasNextInt()) {
+                        parts.add(scanner.next());
+                    } else {
+                        parts.add(scanner.findInLine("\"[^\"]*\"").replace("\"", ""));  // Extracts quoted strings
+                    }
+                }
 
-            // Display the read arrays
-            System.out.println("Read from file:");
-            System.out.println("Integer Array 1: " + Arrays.toString(intArr1));
-            System.out.println("Integer Array 2: " + Arrays.toString(intArr2));
-            System.out.println("String Array 1: " + Arrays.toString(strArr1));
-            System.out.println("String Array 2: " + Arrays.toString(strArr2));
+                scanner.close();
+
+                if (parts.size() > 0 && !parts.get(0).isEmpty()) intList1.add(Integer.parseInt(parts.get(0)));
+                if (parts.size() > 1 && !parts.get(1).isEmpty()) intList2.add(Integer.parseInt(parts.get(1)));
+                if (parts.size() > 2 && !parts.get(2).isEmpty()) strList1.add(parts.get(2));
+                if (parts.size() > 3 && !parts.get(3).isEmpty()) strList2.add(parts.get(3));
+            }
+
+            // Convert lists back to arrays
+            intArr1 = intList1.stream().mapToInt(i -> i).toArray();
+            intArr2 = intList2.stream().mapToInt(i -> i).toArray();
+            strArr1 = strList1.toArray(new String[0]);
+            strArr2 = strList2.toArray(new String[0]);
+            System.out.println("Schedule Found");
         } catch (IOException e) {
             System.err.println("Error reading from file: " + e.getMessage());
         }
     }
 
-    private static int[] parseIntArray(String line) {
-        return Arrays.stream(line.replaceAll("[\\[\\]]", "").split(", "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+    //Redo the time display to include ':' between the hours and minutes
+    //There may also be an error here where if there is a leading zero it mya try to display part of the next time
+    public void display() {
+        for (int i = 0; i < intArr1.length; i ++) {
+            System.out.println("Appointment " + (i + 1) + ": ");
+            System.out.println("  Date: " + (intArr1[i] / 1000000) + '/' + ((intArr1[i] / 10000) % 100) + '/' + (intArr1[i] % 10000));
+            System.out.println("  Time: " + timeConversion("start", intArr2[i]) + '-' + timeConversion("end", intArr2[i]));
+            System.out.println("  Name: " + strArr1[i]);
+            System.out.println("  Description: " + strArr2[i]);
+        }
     }
 
-    private static String[] parseStringArray(String line) {
-        return line.replaceAll("[\\[\\]]", "").split(", ");
+
+
+
+    //These are tested and they work
+    //Consider returning string instead to preserve leading '0'
+    public int timeConversion(int start, int end) {
+        return (start * 10000) + end;
     }
+    public static int timeConversion(String a, int time) {
+        //If stirng equals start return start time but if it equals end then return end time
+        if (a == "start") {
+            return (time / 10000);
+        } if (a == "end") {
+            return (time % 10000);
+        } else {
+            //In case of failure return this to let user know of failure
+            return -1;
+        }
+    }
+
+}
+
+class Menu {
+
+    private FileManger file = new FileManger();
+
+    public Menu() {
+        file.readArraysFromFile();
+    }
+
+    public void end() {
+        file.writeArraysToFile();
+    }
+
+    public void selector(int input) {
+
+        if(input == 1) {
+
+        } if(input == 2) {
+            
+        } if(input == 3) {
+            
+        } if(input == 4) {
+            file.display();
+        } if(input == 5) {
+            System.out.println("Program ending");
+        } else {
+
+        }
+
+    }
+
+}
+
+
+public class filehandler {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+
+        Scanner sc = new Scanner(System.in);
+        int input = 0;
+
+        //Consider useing char instead of int
+        while (input != 5) {
+            System.out.println("To add an event press 1. To delete an even press 2, To rescheudle an event press 3, To display all events press 4, To save schedule and end program enter 5");
+            input = sc.nextInt();
+            menu.selector(input);
+        }
+
+        //Saving data
+        menu.end();
+        sc.close();
+    
+
+    }
+    
 }
